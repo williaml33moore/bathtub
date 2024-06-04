2317bf86f54d2d33890e1a07a8c819b2cfb567ae
Bill Moore <williaml33moore@gmail.com>
2024-06-04 16:18:04 -0400

    Run SVUnit with qrun
    - It works now

--- test/bathtub_pkg_test_suite/pkg_test.py
+++ test/bathtub_pkg_test_suite/pkg_test.py
@@ -1,18 +1,16 @@
 import subprocess
-# import os.path
 import pytest
 
-# @pytest.fixture(params=[""])
-# def run_cmd(request):
-#     """Return absolute path to run script."""
-#     cwd = os.path.dirname(__file__)
-#     run_cmd_param = request.param
-#     return os.path.join(cwd, run_cmd_param)
-
-def test(tmp_path):
+def test_xrun(tmp_path):
     vip_cmd = "xrun $BATHTUB_VIP_DIR/vip-spec.sv"
     assert subprocess.run(vip_cmd, shell=True, cwd=tmp_path).returncode == 0
-    svunit_cmd = "runSVUnit -U -o " + str(tmp_path)
+    svunit_cmd = "runSVUnit --sim xrun --uvm --c_arg '-uvmnocdnsextra' --out " + str(tmp_path)
+    assert subprocess.run(svunit_cmd, shell=True).returncode == 0
+
+def test_qrun(tmp_path):
+    vip_cmd = "qrun $BATHTUB_VIP_DIR/vip-spec.sv"
+    assert subprocess.run(vip_cmd, shell=True, cwd=tmp_path).returncode == 0
+    svunit_cmd = "runSVUnit --sim qrun --uvm --out " + str(tmp_path)
     assert subprocess.run(svunit_cmd, shell=True).returncode == 0
 
 if __name__ == "__main__":
--- test/bathtub_pkg_test_suite/svunit.f
+++ test/bathtub_pkg_test_suite/svunit.f
@@ -1,5 +1,4 @@
 // svunit.f
 -timescale 1ns/1ns
 -uvmhome $UVM_HOME
--uvmnocdnsextra
 -f bathtub_vip.f
