818996b357b7e55af0e83cdfab828e3552465ea5
Bill Moore <williaml33moore@gmail.com>
2024-06-06 08:27:46 -0400

    Test bathtub_macros.sv dependencies
    - Some of the classes in bathub_pkg are primarily used by macros in bathtub_macros.sv, so test that all the macro requirements are satisfied.

--- test/dependency_test.py
+++ test/dependency_test.py
@@ -57,3 +57,54 @@ testbench.sv \
 #
 """
     assert subprocess.run(run_cmd, shell=True, cwd=tmp_path).returncode == 0
+
+def test_macro_dependencies(tmp_path, simulator):
+    """Test that bathtub_macros dependencies are satisfied by bathtub_pkg"""
+    # Bathtub_macros doesn't include or import any depencies on its own.
+    # Ensure that compiling bathtub_pkg satisfies bathtub_macros' requirements.
+    class_under_test = 'bathtub_macros.sv'
+    f = tmp_path / 'testbench.sv'
+    content = "\n".join(['program test();',
+                         '`include "' + class_under_test + '"',
+                        'endprogram : test'
+                        ])
+    f.write_text(content, encoding="utf-8")
+    run_cmd = simulator + " " + """\
+-uvm \
+-uvmhome $UVM_HOME \
+$BATHTUB_VIP_DIR/src/gherkin_pkg/gherkin_pkg.sv \
+$BATHTUB_VIP_DIR/src/bathtub_pkg/bathtub_pkg.sv \
+-incdir $BATHTUB_VIP_DIR/src/gherkin_pkg \
+-incdir $BATHTUB_VIP_DIR/src/bathtub_pkg \
+-incdir $BATHTUB_VIP_DIR/src/bathtub_pkg/gherkin_document_printer \
+-incdir $BATHTUB_VIP_DIR/src/bathtub_pkg/gherkin_document_runner \
+-incdir $BATHTUB_VIP_DIR/src/bathtub_pkg/gherkin_parser \
+testbench.sv \
+#
+"""
+    assert subprocess.run(run_cmd, shell=True, cwd=tmp_path).returncode == 0
+
+def test_macro_command_line_dependencies(tmp_path, simulator):
+    """Test that bathtub_macros can be compiled from the command line"""
+    # The file is called "bathtub_macros.sv," not "bathtub_macros.svh," so ensure it can be compiled from the command line. 
+    f = tmp_path / 'testbench.sv'
+    content = "\n".join(['program test();',
+                         '; // Empty',
+                        'endprogram : test'
+                        ])
+    f.write_text(content, encoding="utf-8")
+    run_cmd = simulator + " " + """\
+-uvm \
+-uvmhome $UVM_HOME \
+$BATHTUB_VIP_DIR/src/gherkin_pkg/gherkin_pkg.sv \
+$BATHTUB_VIP_DIR/src/bathtub_pkg/bathtub_pkg.sv \
+$BATHTUB_VIP_DIR/src/bathtub_pkg/bathtub_macros.sv \
+-incdir $BATHTUB_VIP_DIR/src/gherkin_pkg \
+-incdir $BATHTUB_VIP_DIR/src/bathtub_pkg \
+-incdir $BATHTUB_VIP_DIR/src/bathtub_pkg/gherkin_document_printer \
+-incdir $BATHTUB_VIP_DIR/src/bathtub_pkg/gherkin_document_runner \
+-incdir $BATHTUB_VIP_DIR/src/bathtub_pkg/gherkin_parser \
+testbench.sv \
+#
+"""
+    assert subprocess.run(run_cmd, shell=True, cwd=tmp_path).returncode == 0
