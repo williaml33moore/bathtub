7fc7b329fb615c5f4976e00947c66b7c6f127253
Bill Moore <williaml33moore@gmail.com>
2024-06-06 20:57:27 -0400

    New task parse_feature_lines()
    - modified:   src/bathtub_pkg/gherkin_parser/gherkin_parser.svh
    - modified:   src/bathtub_pkg/gherkin_parser/gherkin_parser_interface.svh
      - New task parses an array of strings, as opposed to a file
    
    - modified:   test/bathtub_pkg_test_suite/gherkin_parser_unit_test.sv
      - New unit test for new parser task
    
    -modified:   test/bathtub_pkg_test_suite/pkg_test.py
      - SVUnit doesn't currently cause simulator to fail, so added a check to the pytest that greps the log file for a positive "PASSED" message.

--- test/bathtub_pkg_test_suite/gherkin_parser_unit_test.sv
+++ test/bathtub_pkg_test_suite/gherkin_parser_unit_test.sv
@@ -1,5 +1,6 @@
 `include "svunit_defines.svh"
   import bathtub_pkg::gherkin_parser;
+  import bathtub_pkg::gherkin_doc_bundle;
 
 module gherkin_parser_unit_test;
   import svunit_pkg::svunit_testcase;
@@ -12,7 +13,7 @@ module gherkin_parser_unit_test;
   // This is the UUT that we're 
   // running the Unit Tests on
   //===================================
-  gherkin_parser my_gherkin_parser;
+  gherkin_parser parser;
 
 
   //===================================
@@ -21,7 +22,6 @@ module gherkin_parser_unit_test;
   function void build();
     svunit_ut = new(name);
 
-    my_gherkin_parser = new(/* New arguments if needed */);
   endfunction
 
 
@@ -32,6 +32,7 @@ module gherkin_parser_unit_test;
     svunit_ut.setup();
     /* Place Setup Code Here */
 
+    parser = new("parser");
   endtask
 
 
@@ -61,6 +62,44 @@ module gherkin_parser_unit_test;
   //===================================
   `SVUNIT_TESTS_BEGIN
 
+  `SVTEST(Task_parse_feature_lines_should_parse_a_minimal_feature_array)
+  // ===================================================================
+  string feature[];
+  gherkin_doc_bundle actual_doc_bundle;
+	string actual_file_name, expected_file_name;
+  gherkin_pkg::feature actual_feature;
+  gherkin_pkg::scenario actual_scenario;
+  gherkin_pkg::step actual_step;
+
+  feature = {
+    "Feature: This is a feature",
+    "Scenario: This is a scenario",
+    "* This is a step"
+  };
+
+  parser.parse_feature_lines(feature, actual_doc_bundle);
+  `FAIL_UNLESS(actual_doc_bundle)
+
+  actual_file_name = actual_doc_bundle.file_name;
+  expected_file_name = "";
+  `FAIL_UNLESS_STR_EQUAL(actual_file_name, expected_file_name)
+  
+  actual_feature = actual_doc_bundle.document.get_as_value().feature;
+  `FAIL_UNLESS_STR_EQUAL(actual_feature.get_as_value().keyword, "Feature")
+  `FAIL_UNLESS_STR_EQUAL(actual_feature.get_as_value().feature_name, "This is a feature")
+  
+  `FAIL_UNLESS_EQUAL(actual_feature.get_as_value().scenario_definitions.size(), 1)
+  `FAIL_UNLESS($cast(actual_scenario, actual_feature.get_as_value().scenario_definitions[0]))
+
+  `FAIL_UNLESS_STR_EQUAL(actual_scenario.get_as_value().base.keyword, "Scenario")
+  `FAIL_UNLESS_STR_EQUAL(actual_scenario.get_as_value().base.scenario_definition_name, "This is a scenario")
+  `FAIL_UNLESS_EQUAL(actual_scenario.get_as_value().base.steps.size(), 1)
+  
+  actual_step = actual_scenario.get_as_value().base.steps[0];
+  `FAIL_UNLESS_STR_EQUAL(actual_step.get_as_value().keyword, "*")
+  `FAIL_UNLESS_STR_EQUAL(actual_step.get_as_value().text, "This is a step")
+	
+  `SVTEST_END
 
 
   `SVUNIT_TESTS_END
