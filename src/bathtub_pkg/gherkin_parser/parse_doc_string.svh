4880aa3b13c2194594adb7792e22fc0fdb5c6b4c
Bill Moore <118405419+williaml33moore@users.noreply.github.com>
2024-06-08 16:43:11 -0700

    Implement parsing doc strings (#46)
    Closes issue #45.
    
    - modified:   src/bathtub_pkg/gherkin_parser/parse_doc_string.svh
      - Built out `parse_doc_string()` placeholder stub.
    
    - modified:   test/bathtub_pkg_test_suite/gherkin_parser_unit_test.sv
      - New test `Parse_a_doc_string`
      - New test `Parse_a_doc_string_that_contains_white_space`
    
    - modified:   src/bathtub_pkg/gherkin_parser/gherkin_parser.svh
      - Added `preserve_white_space` option to `get_new_line()` because doc strings should preserve all leading and trailing white space and empty lines.
    
    - modified:   src/bathtub_pkg/gherkin_parser/parse_data_table.svh
      - Explicitly delete the `data_table_value` rows, just to make the intent clear.

--- src/bathtub_pkg/gherkin_parser/parse_doc_string.svh
+++ src/bathtub_pkg/gherkin_parser/parse_doc_string.svh
@@ -26,7 +26,83 @@ SOFTWARE.
 `define __PARSE_DOC_STRING_SVH
 
 task gherkin_parser::parse_doc_string(ref gherkin_pkg::doc_string doc_string);
-	`uvm_fatal("PENDING", "")
+	line_value line_obj;
+	line_analysis_result_t line_analysis_result;
+	gherkin_pkg::doc_string_value doc_string_value;
+	string delimiter = "";
+
+	line_mbox.peek(line_obj);
+
+	`uvm_info_begin(`BATHTUB__GET_SCOPE_NAME(), "gherkin_parser::parse_doc_string enter", UVM_HIGH)
+	`uvm_message_add_string(line_obj.file_name)
+	`uvm_message_add_int(line_obj.line_number, UVM_DEC)
+	`uvm_message_add_int(line_obj.eof, UVM_BIN)
+	if (!line_obj.eof) begin
+		`uvm_message_add_string(line_obj.text)
+	end
+	`uvm_info_end
+	`uvm_info(`BATHTUB__GET_SCOPE_NAME(), $sformatf("parser_stack: %p", parser_stack), UVM_HIGH)
+
+	if (!line_obj.eof) begin
+
+		analyze_line(line_obj.text, line_analysis_result);
+
+		case (line_analysis_result.secondary_keyword)
+			"\"\"\"", "```" : begin : configure_doc_string
+				delimiter = line_analysis_result.secondary_keyword;
+
+				doc_string_value.content_type = line_analysis_result.remainder_after_secondary_keyword;
+				doc_string_value.content = "";
+
+				get_next_line(line_obj);
+
+				while (status == OK) begin : content
+					line_mbox.peek(line_obj);
+
+					if (line_obj.eof) break;
+
+					analyze_line(line_obj.text, line_analysis_result);
+
+					case (line_analysis_result.secondary_keyword)
+						delimiter : begin : terminate
+							// Matching delimiter terminates the doc string
+
+							if (line_analysis_result.remainder_after_secondary_keyword.len() != 0) begin
+								status = ERROR;
+								`uvm_error(`BATHTUB__GET_SCOPE_NAME(), {"Unexpected content type after closing delimiter: ", line_analysis_result.remainder_after_secondary_keyword,
+									". Expecting nothing."})
+							end
+
+							get_next_line(line_obj);
+							break;
+						end
+
+						default : begin : construct_content
+							// Newline gets trimmed by parser, so append a new one.
+							doc_string_value.content = {doc_string_value.content, line_obj.text, "\n"};
+							// Preserve all white space in a doc string
+							get_next_line(.line_obj(line_obj), .preserve_white_space(1'b1));
+						end
+					endcase
+				end
+			end
+
+			default : begin
+				status = ERROR;
+				`uvm_error(`BATHTUB__GET_SCOPE_NAME(), {"Unexpected keyword: ", line_analysis_result.secondary_keyword,
+					". Expecting \"\"\" or ```"})
+			end
+		endcase
+	end
+
+	doc_string = new("data_table", doc_string_value);
+	`push_onto_parser_stack(doc_string)
+
+	`uvm_info_begin(`BATHTUB__GET_SCOPE_NAME(), "gherkin_parser::parse_doc_string exit", UVM_HIGH)
+	`uvm_message_add_tag("status", status.name())
+	`uvm_message_add_object(doc_string)
+	`uvm_info_end
+	`uvm_info(`BATHTUB__GET_SCOPE_NAME(), $sformatf("parser_stack: %p", parser_stack), UVM_HIGH)
 endtask : parse_doc_string
 
 `endif // __PARSE_DOC_STRING_SVH
