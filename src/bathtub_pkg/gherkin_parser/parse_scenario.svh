284106697c36e1ef18d1c2570d4f4218e8a13f08
Bill Moore <williaml33moore@gmail.com>
2024-06-10 12:03:17 -0700

    Implement parsing tags
        Works for a single tag on a scenario
    
    - modified:   src/bathtub_pkg/gherkin_parser/parse_feature.svh
    - modified:   src/bathtub_pkg/gherkin_parser/parse_scenario.svh
    - modified:   test/bathtub_pkg_test_suite/gherkin_parser_unit_test.sv
    
    - modified:   src/bathtub_pkg/gherkin_parser/parse_background.svh
      - Backgrounds can't have tags, so flag an error.

--- src/bathtub_pkg/gherkin_parser/parse_scenario.svh
+++ src/bathtub_pkg/gherkin_parser/parse_scenario.svh
@@ -53,6 +53,9 @@ task gherkin_parser::parse_scenario(ref gherkin_pkg::scenario scenario);
 
 				scenario_value.base.keyword = line_analysis_result.token_before_colon;
 				scenario_value.base.scenario_definition_name = line_analysis_result.remainder_after_colon;
+				while (floating_tags.size() > 0) begin
+					scenario_value.tags.push_back(floating_tags.pop_front());
+				end
 				
 				get_next_line(line_obj);
 
