a5f17f3789b9b2c36aa84ac2202d4edaa75487bc
Bill Moore <williaml33moore@gmail.com>
2024-06-11 02:58:35 -0700

    Add verbosity option
    - modified:   test/bathtub_pkg_test_suite/pkg_test.py
      - Add `UVM_VERBOSITY` option.
      - Commented out for now.
      - Will eventually make this a fixture option available from the command-line

--- test/bathtub_pkg_test_suite/pkg_test.py
+++ test/bathtub_pkg_test_suite/pkg_test.py
@@ -5,7 +5,7 @@ def test_xrun(tmp_path):
     vip_cmd = "xrun $BATHTUB_VIP_DIR/vip-spec.sv"
     assert subprocess.run(vip_cmd, shell=True, cwd=tmp_path).returncode == 0
     svunit_cmd = "runSVUnit --sim xrun --uvm --c_arg '-uvmnocdnsextra' --out " + str(tmp_path)
-    svunit_cmd += " -r +UVM_VERBOSITY=UVM_HIGH"
+    # svunit_cmd += " -r +UVM_VERBOSITY=UVM_HIGH"
     assert subprocess.run(svunit_cmd, shell=True).returncode == 0
     checker_cmd = "grep '^INFO.*testrunner.*PASSED' " + str(tmp_path) + "/run.log"
     assert subprocess.run(checker_cmd, shell=True).returncode == 0
@@ -14,7 +14,7 @@ def test_qrun(tmp_path):
     vip_cmd = "qrun $BATHTUB_VIP_DIR/vip-spec.sv"
     assert subprocess.run(vip_cmd, shell=True, cwd=tmp_path).returncode == 0
     svunit_cmd = "runSVUnit --sim qrun --uvm --out " + str(tmp_path)
-    svunit_cmd += " -r +UVM_VERBOSITY=UVM_HIGH"
+    # svunit_cmd += " -r +UVM_VERBOSITY=UVM_HIGH"
     assert subprocess.run(svunit_cmd, shell=True).returncode == 0
     checker_cmd = "grep '^# INFO.*testrunner.*PASSED' " + str(tmp_path) + "/run.log"
     assert subprocess.run(checker_cmd, shell=True).returncode == 0
