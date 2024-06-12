e1a09148d540442d65ec1a0fba5d194867df16df
Bill Moore <williaml33moore@gmail.com>
2024-06-12 10:41:25 -0700

    Questa works
    - modified:   test/bathtub_pkg_test_suite/pkg_test.py

--- test/bathtub_pkg_test_suite/pkg_test.py
+++ test/bathtub_pkg_test_suite/pkg_test.py
@@ -5,8 +5,6 @@ class Simulator:
     def __init__(self):
         self.args = []
         self.returncode = -1
-        self.binary = 'xrun'
-        self.name = 'xcelium'
     
     def append_args(self, arg):
         self.args.append(arg)
@@ -20,6 +18,18 @@ class Simulator:
     def passed(self):
         return self.returncode == 0
     
+    def name(self):
+        return self.__class__.__name__
+
+class Xcelium(Simulator):
+    def __init__(self):
+        super().__init__()
+        self.binary = 'xrun'
+
+class Questa(Simulator):
+    def __init__(self):
+        super().__init__()
+        self.binary = 'qrun'
 
 class SVUnit:
     def __init__(self):
@@ -31,15 +41,19 @@ class SVUnit:
         self.returncode = -1
 
     def sim(self, simulator):
+        sim_arg_values = {
+            'Xcelium' : 'xcelium',
+            'Questa' : 'qrun'
+        }
         self.simulator = simulator
-        self.args.append('--sim ' + self.simulator.name)
+        self.args.append('--sim ' + sim_arg_values[self.simulator.name()])
         return self
     
     def uvm(self, uvm_flag=True):
         self.uvm_flag = uvm_flag
         if self.uvm_flag:
             self.args.append('--uvm')
-        if self.simulator.name == 'xcelium':
+        if self.simulator.name() == 'Xcelium':
             self.args.append("--c_arg '-uvmnocdnsextra'")
         return self
     
@@ -48,12 +62,19 @@ class SVUnit:
         self.args.append('--out ' + str(output_dir))
         return self
     
+    def check_testrunner_result(self, cwd='.'):
+        if self.simulator.name() == 'Questa':
+            checker_cmd = "grep '^# INFO.*testrunner.*PASSED' " + str(self.output_dir) + "/run.log"
+        else:
+            checker_cmd = "grep '^INFO.*testrunner.*PASSED' " + str(self.output_dir) + "/run.log"
+        self.returncode = subprocess.run(checker_cmd, shell=True, cwd=cwd).returncode
+
+    
     def run(self, cwd='.'):        
         svunit_cmd = " ".join([self.script] + self.args)
         self.returncode = subprocess.run(svunit_cmd, shell=True, cwd=cwd).returncode
         if self.returncode == 0:
-            checker_cmd = "grep '^INFO.*testrunner.*PASSED' " + str(self.output_dir) + "/run.log"
-            self.returncode = subprocess.run(checker_cmd, shell=True, cwd=cwd).returncode
+            self.check_testrunner_result(cwd)
         return self
     
     def passed(self):
@@ -62,7 +83,8 @@ class SVUnit:
 
 @pytest.fixture
 def simulator():
-    sim = Simulator()
+    # sim = Xcelium()
+    sim = Questa()
     return sim
 
 def test_xrun(tmp_path):
