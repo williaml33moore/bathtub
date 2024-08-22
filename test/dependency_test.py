f02a96a02fdc8481207dd9f58db69f868f1aafc6
Bill Moore <williaml33moore@gmail.com>
2024-08-22 15:43:39 -0700

    Roll back changes.
    - modified:   test/dependency_test.py
      - No test changes were needed in previous commit.

--- test/dependency_test.py
+++ test/dependency_test.py
@@ -23,37 +23,37 @@
 import pytest
 
 classes_under_test = [
-    "bathtub_pkg/bathtub_pkg.svh",
     "bathtub_pkg/bathtub.svh",
+    "bathtub_pkg/bathtub_pkg.svh",
     "bathtub_pkg/bathtub_utils.svh",
     "bathtub_pkg/context_sequence.svh",
-    "bathtub_pkg/feature_sequence_interface.svh",
     "bathtub_pkg/feature_sequence.svh",
+    "bathtub_pkg/feature_sequence_interface.svh",
     "bathtub_pkg/gherkin_doc_bundle.svh",
-    "bathtub_pkg/gherkin_step_bundle.svh",
-    "bathtub_pkg/line_value.svh",
-    "bathtub_pkg/plusarg_options.svh",
-    "bathtub_pkg/pool_provider_interface.svh",
-    "bathtub_pkg/pool_provider.svh",
-    "bathtub_pkg/rule_sequence_interface.svh",
-    "bathtub_pkg/rule_sequence.svh",
-    "bathtub_pkg/scenario_sequence_interface.svh",
-    "bathtub_pkg/scenario_sequence.svh",
-    "bathtub_pkg/snippets.svh",
-    "bathtub_pkg/step_attributes_interface.svh",
-    "bathtub_pkg/step_definition_seq.svh", 
-    "bathtub_pkg/step_definition_interface.svh",
-    "bathtub_pkg/step_nature.svh",
-    "bathtub_pkg/step_nurture.svh",
-    "bathtub_pkg/step_parameter_arg.svh",
-    "bathtub_pkg/step_parameters_interface.svh",
-    "bathtub_pkg/step_parameters.svh",
-    "bathtub_pkg/step_static_attributes_interface.svh",
-    "bathtub_pkg/test_sequence_interface.svh",
-    "bathtub_pkg/test_sequence.svh",
     "bathtub_pkg/gherkin_document_printer/gherkin_document_printer.svh",
     "bathtub_pkg/gherkin_document_runner/gherkin_document_runner.svh",
     "bathtub_pkg/gherkin_parser/gherkin_parser.svh",
+    "bathtub_pkg/gherkin_step_bundle.svh",
+    "bathtub_pkg/line_value.svh",
+    "bathtub_pkg/plusarg_options.svh",
+    "bathtub_pkg/pool_provider.svh",
+    "bathtub_pkg/pool_provider_interface.svh",
+    "bathtub_pkg/rule_sequence.svh",
+    "bathtub_pkg/rule_sequence_interface.svh",
+    "bathtub_pkg/scenario_sequence.svh",
+    "bathtub_pkg/scenario_sequence_interface.svh",
+    "bathtub_pkg/snippets.svh",
+    "bathtub_pkg/step_attributes_interface.svh",
+    "bathtub_pkg/step_definition_interface.svh",
+    "bathtub_pkg/step_definition_seq.svh", 
+    "bathtub_pkg/step_nature.svh",
+    "bathtub_pkg/step_nurture.svh",
+    "bathtub_pkg/step_parameter_arg.svh",
+    "bathtub_pkg/step_parameters.svh",
+    "bathtub_pkg/step_parameters_interface.svh",
+    "bathtub_pkg/step_static_attributes_interface.svh",
+    "bathtub_pkg/test_sequence.svh",
+    "bathtub_pkg/test_sequence_interface.svh",
 ]
 
 @pytest.fixture(params=classes_under_test)
@@ -79,31 +79,6 @@ def test_class_dependencies(tmp_path, simulator, class_under_test):
         'testbench.sv',
         ])
     assert simulator.run(tmp_path).passed()
-
-# @pytest.mark.parametrize("class_under_test", [
-#     ])
-# def test_step_definition_dependencies(tmp_path, simulator, class_under_test):
-#     """Test that each step_definition_seq fully specifies all its dependencies"""
-#     # Create a minimal top level that just includes the class file.
-#     # Simulate it with only required packages and include directories.
-#     # Class file should include everything it needs and compile successfully.
-#     # This is a special case.
-#     # A few classe reside in the bathtub_pkg but they are used outside the package.
-#     # These classes depend on other classes in the package, so we must compile the package.
-#     f = tmp_path / 'testbench.sv'
-#     content = "\n".join(['program test();',
-#                          '`include "' + class_under_test + '"',
-#                         'endprogram : test'
-#                         ])
-#     f.write_text(content, encoding="utf-8")
-#     simulator.uvm()
-#     simulator.extend_args([
-#         '$BATHTUB_VIP_DIR/src/gherkin_pkg/gherkin_pkg.sv',
-#         '$BATHTUB_VIP_DIR/src/bathtub_pkg/bathtub_pkg.sv',
-#         '-incdir $BATHTUB_VIP_DIR/src',
-#         'testbench.sv',
-#         ])
-#     assert simulator.run(tmp_path).passed()
 
 def test_macro_dependencies(tmp_path, simulator):
     """Test that bathtub_macros dependencies are satisfied by bathtub_pkg"""
